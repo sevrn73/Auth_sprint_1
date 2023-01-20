865d8842f51520884687a94a1f592f25b2046686
sevrn73 <57312403+sevrn73@users.noreply.github.com>
2023-01-21 01:00:18 +0300

    readme

--- auth_api/src/db/db.py
+++ auth_api/src/db/db.py
@@ -5,7 +5,7 @@ from src.core.config import db_settings
 
 
 db = SQLAlchemy()
-SQLALCHEMY_DATABASE_URI = f'postgresql://{db_settings.user}:{db_settings.host}@{":".join((db_settings.host, str(db_settings.port)))}/{db_settings.dbname}'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{db_settings.user}:{db_settings.password}@{":".join((db_settings.host, str(db_settings.port)))}/{db_settings.dbname}'
 
 
 def init_db(app: Flask):
